dd17dd5c1118c4a261283d2680699e1412c9eb64
giladyosef54 <57717590+giladyosef54@users.noreply.github.com>
2024-02-28 10:50:09 +0200

    finish server

--- app/server/server.py
+++ app/server/server.py
@@ -1,17 +1,38 @@
 from flask import Flask, request
 from dotenv import load_dotenv
 from os import getenv
+from ipaddress import ip_address
+from collections import deque
 
 load_dotenv()
 
 
+
 app = Flask(__name__)
-subnet_mask = getenv('SUBNET_MASK')
+
+subnet_mask = int(ip_address(getenv('SUBNET_MASK')))
+
+broadcast = int(ip_address('255.255.255.255'))
+unused_ip = subnet_mask & broadcast
 
 
-@app.get('/get_ip')
+def gen_ip():
+    global unused_ip
+    if unused_ip + 1 + subnet_mask == broadcast:
+        return None
+    else:
+        unused_ip += 1
+        return unused_ip
+
+
+
+@app.get('/getIP')
 def get_ip():
-    pass
+    ip = gen_ip()
+    if ip is None:
+        return 'There is no available IPs', 400
+    else:
+        return str(ip_address(ip)), 200
 
 
 def main():
